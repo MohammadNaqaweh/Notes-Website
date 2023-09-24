bcaf3e91ec790526b64d48490ec8a2cb8c32a011
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-09-25 01:43:41 +0300

    Added Shadow to Header on scroll

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LeftSection, MiddleSection, RightSection } from '../../components/index.js';
 import './header.css';
 
 const iconSize = 20;
 
 const Header = ( {isHome, pageTitle} ) => {
-  const [ isGrid, setIsGrid ] = useState(false);
+  const [ top, setTop ] = useState(true);
   
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.scrollY > 10 ? setTop(false) : setTop(true);
+    };
+
+    window.addEventListener('scroll', scrollHandler);
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, [top])
+
   return (
-    <div className='keep-notes__header'>
+    <div className={`keep-notes__header ${!top && 'bottom-box-shadow'}`}>
       <LeftSection isHome={isHome} pageTitle={pageTitle} iconSize={iconSize}/>
       <MiddleSection iconSize={iconSize} />
       <RightSection iconSize={iconSize}/>
