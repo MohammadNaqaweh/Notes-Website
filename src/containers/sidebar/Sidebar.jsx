ab404cb1baa17c84e36453d6efc7e84885061f88
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-09-27 18:30:11 +0300

    Change Sidevar button to dynamiclly render from Array

--- src/containers/sidebar/Sidebar.jsx
+++ src/containers/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { LiaLightbulb } from 'react-icons/lia';
 import { AiOutlineBell } from 'react-icons/ai';
 import { GoPencil } from 'react-icons/go';
@@ -7,30 +7,39 @@ import './sidebar.css';
 
 const Sidebar = () => {
   const iconSize = 24;
-  
+
+  const btnData = [
+    { 
+      icon: <LiaLightbulb size={iconSize}/>,
+      text: "Notes"
+    },
+    {
+      icon: <AiOutlineBell size={iconSize} />,
+      text: "Reminders"
+    },
+    {
+      icon: <GoPencil size={iconSize} />,
+      text: "Edit labels"
+    },
+    {
+      icon: <PiArchiveBox size={iconSize} />,
+      text: "Archive"
+    },
+    {
+      icon: <PiTrash size={iconSize} />,
+      text: "Trash"
+    }
+  ]
+
   return (
     <div className='keep-notes__sidebar'>
       <div className='keep-notes__sidebar-buttons_container'>
-        <div className='keep-notes__sidebar-button'>
-          <div className='keep-notes__sidebar-button_icon'> <LiaLightbulb size={iconSize} /></div>
-          <p>Notes</p>
-        </div>
-        <div className='keep-notes__sidebar-button'>
-          <div className='keep-notes__sidebar-button_icon'><AiOutlineBell size={iconSize} /></div>
-          <p>Reminders</p>
-        </div>
-        <div className='keep-notes__sidebar-button'> 
-          <div className='keep-notes__sidebar-button_icon'><GoPencil size={iconSize} /></div>
-          <p>Edit labels</p>
-        </div>
-        <div className='keep-notes__sidebar-button'>
-          <div className='keep-notes__sidebar-button_icon'><PiArchiveBox size={iconSize} /></div>
-          <p>Archive</p>
-        </div>
-        <div className='keep-notes__sidebar-button'>
-          <div className='keep-notes__sidebar-button_icon'><PiTrash size={iconSize} /></div>
-          <p>trash</p>
-        </div>
+        {btnData.map((item,index)=>(
+          <div className='keep-notes__sidebar-button'>
+            <div className='keep-notes__sidebar-button_icon'>{item.icon}</div>
+            <p>{item.text}</p>
+          </div>
+        ))}
       </div>
       <div className='keep-notes__sidebar-footer'>
         <a href='https://keep.google.com/' target='_blank'>Open-source licenses</a>
